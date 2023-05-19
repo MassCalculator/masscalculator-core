324214f1c7fb357cb61fd7ab92e14586c0523586
Mergim Halimi <m.halimi123@gmail.com>
2023-05-19 05:01:08 +0200

    Fix typo in doxy

--- third_party/doxygen.cmake
+++ third_party/doxygen.cmake
@@ -2,6 +2,6 @@ cmake_minimum_required(VERSION 3.15)
 
 set(DOXYGEN_REQUIRED_VERSION 1.8.17)
 
-# Check that GoogleTest is installed in the system.
+# Check that Doxygen is installed in the system.
 # This call cannot fail.
 find_package(Doxygen ${DOXYGEN_REQUIRED_VERSION} REQUIRED)
